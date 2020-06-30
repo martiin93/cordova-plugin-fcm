e928cfdc40e42ae206cd0d53ff3ae40b7ef01f6f
martiin93 <67595275+martiin93@users.noreply.github.com>
2020-06-30 14:23:33 +0200

    Add files via upload

---
+++ FCMPlugin.h
@@ -0,0 +1,21 @@
+#import <UIKit/UIKit.h>
+#import <Cordova/CDVPlugin.h>
+
+@interface FCMPlugin : CDVPlugin
+{
+    //NSString *notificationCallBack;
+}
+
++ (FCMPlugin *) fcmPlugin;
+- (void)ready:(CDVInvokedUrlCommand*)command;
+- (void)getToken:(CDVInvokedUrlCommand*)command;
+- (void)subscribeToTopic:(CDVInvokedUrlCommand*)command;
+- (void)unsubscribeFromTopic:(CDVInvokedUrlCommand*)command;
+- (void)onNotification:(CDVInvokedUrlCommand*)command;
+- (void)notifyOfMessage:(NSData*) payload;
+- (void)notifyOfTokenRefresh:(NSString*) token;
+- (void)logEvent:(CDVInvokedUrlCommand*)command;
+
+//#define AppDelegateSingleton ((AppDelegate *)[[UIApplication sharedApplication] delegate])
+
+@end
